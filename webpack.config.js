d0201c31ffc0cbbbc11ff947b8110a06ef5286b2
davidkatz <15Dkatz@shcp.edu>
2017-02-04 11:48:05 -0800

    ignore modules

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ['babel-polyfill', './app/index.js'],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // a regular expression that catches .js files
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000, // most common port
+    contentBase: './build',
+    inline: true
+  }
+}
